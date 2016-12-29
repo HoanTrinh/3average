6af2336004a97706f3470b33e1fc7152a8194bf7
HoanTrinh <hoantrinh91@gmail.com>
2016-12-29 13:50:06 -0500

    Add files via upload

---
+++ 3averagenumber.js
@@ -0,0 +1,17 @@
+var num1, num2, num3;
+
+
+ console.log('Num1: ', num1);
+ console.log('Num2: ', num2);
+ console.log('Num3: ', num3);
+ def average:
+    total = 0
+    for subject in num1,num2,num3:
+        total += num1*num2*num3
+    return total
+
+  if(average > num1,num2 ) {
+      function('logout');
+    } else {
+      console.log('the average is low');
+    }
